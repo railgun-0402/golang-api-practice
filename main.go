79860e9654d8ef81b2205c465d173308ae09ac91
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-12-18 23:32:13 +0900

    ハンドラーを増やした分、リファクタリングする

--- main.go
+++ main.go
@@ -1,46 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"go-practice-hands/handlers"
 	"log"
 	"net/http"
 )
 
 func main() {
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		// HelloWorldを書き込む
-		io.WriteString(w, "Hello, World\n")
-	}
 
-	postArticleHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Artcile...\n")
-	}
-
-	articleListHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Article List\n")
-	}
-
-	articleDetailHandler := func(w http.ResponseWriter, req *http.Request) {
-		articleID := 1
-		resString := fmt.Sprintf("Article No.%\n", articleID)
-		io.WriteString(w, resString)
-	}
-
-	articleNiceHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Nice...\n")
-	}
-
-	articleCommentHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Comment...\n")
-	}
-
-	http.HandleFunc("/hello", helloHandler)
-	http.HandleFunc("/article", postArticleHandler)
-	http.HandleFunc("/article/list", articleListHandler)
-	http.HandleFunc("/article/1", articleDetailHandler)
-	http.HandleFunc("/article/nice", articleNiceHandler)
-	http.HandleFunc("/comment", articleCommentHandler)
+	http.HandleFunc("/hello", handlers.HelloHandler)
+	http.HandleFunc("/article", handlers.PostArticleHandler)
+	http.HandleFunc("/article/list", handlers.ArticleListHandler)
+	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	http.HandleFunc("/article/nice", handlers.ArticleNiceHandler)
+	http.HandleFunc("/comment", handlers.ArticleCommentHandler)
 
 	log.Println("server start at port: 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
