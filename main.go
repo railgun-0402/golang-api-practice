f3be47bc250982f4082887971da6ac79150274fd
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2025-02-16 23:36:33 +0900

    dbにデータを挿入した結果をGoで参照する

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"go-practice-hands/models"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -25,9 +26,25 @@ func main() {
 	}
 	defer db.Close()
 
-	if err := db.Ping(); err != nil {
+	const sqlStr = `select title, contents, username, nice from articles;`
+	rows, err := db.Query(sqlStr)
+	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println("connect to DB")
+		return
 	}
+	defer rows.Close()
+
+	articleArr := make([]models.Article, 0)
+	for rows.Next() {
+		var article models.Article
+		err := rows.Scan(&article.Title, &article.Contents, &article.UserName, &article.NiceNum)
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			// 読み出し結果を格納した変数 article を、配列に追加
+			articleArr = append(articleArr, article)
+		}
+	}
+	fmt.Printf("%+v\n", articleArr)
 }
