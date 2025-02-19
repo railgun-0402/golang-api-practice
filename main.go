838484194816358dea1ab784f6d9ffc09211f477
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2025-02-20 01:14:07 +0900

    トランザクションの導入

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"go-practice-hands/models"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -16,52 +17,127 @@ const (
 	dbName = "sampledb"
 )
 
-func main() {
-
+// DB接続
+func connectDB() (*sql.DB, error) {
 	dbConn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
 
 	db, err := sql.Open("mysql", dbConn)
 	if err != nil {
-		fmt.Println(err)
+		return nil, fmt.Errorf("DB接続エラー: %w", err)
 	}
-	defer db.Close()
+	return db, nil
+}
 
-	insertArticle := models.Article {
-		Title: "insert test",
-		Contents: "Can I insert data correctly?",
-		UserName: "seki",
-	}
-
+// 記事の挿入
+func insertArticleFunc(db *sql.DB, article models.Article) (sql.Result, error) {
 	const insertSqlStr = `insert into articles (title, contents, username, nice, created_at) values(?, ?, ?, 0, now());`
-	result, err := db.Exec(insertSqlStr, insertArticle.Title, insertArticle.Contents, insertArticle.UserName)
+	result, err := db.Exec(insertSqlStr, article.Title, article.Contents, article.UserName)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, fmt.Errorf("データのINSERTが失敗しました: %w", err)
 	}
+	return result, nil
+}
 
-	fmt.Println(result.LastInsertId()) // 何番目のデータかを見る
-	fmt.Println(result.RowsAffected()) // DBに何行の変更が行われたか見る
-
-	articleId := 1
+// IDでDBからデータを取得する
+func getArticleByID(db *sql.DB, articleId int) (*models.Article, error) {
 	const sqlStr = `select * from articles where article_id = ?;`
 	row := db.QueryRow(sqlStr, articleId)
 
 	// データ取得件数が0件の場合は終了
 	if err := row.Err(); err != nil {
-		fmt.Println(err)
-		return
+		return nil, fmt.Errorf("データが見つかりませんでした: %w", err)
 	}
 
 	var article models.Article
 	var createdTime sql.NullTime
-	err = row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+	err := row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, fmt.Errorf("データの取得に失敗しました: %w", err)
 	}
 
 	if createdTime.Valid {
 		article.CreatedAt = createdTime.Time
 	}
+
+	return &article, nil
+}
+
+func main() {
+
+	// DBに接続
+	db, err := connectDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// 追加するデータ
+	// insertArticle := models.Article {
+	// 	Title: "insert test",
+	// 	Contents: "Can I insert data correctly?",
+	// 	UserName: "seki",
+	// }
+
+	// データを追加する
+	// result, err := insertArticleFunc(db, insertArticle)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Println(result.LastInsertId()) // 何番目のデータかを見る
+	// fmt.Println(result.RowsAffected()) // DBに何行の変更が行われたか見る
+
+	articleId := 1
+	// IDに紐づくデータを取得する
+	article, err := getArticleByID(db, articleId)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	fmt.Printf("%+v\n", article)
+
+	// データのUpdate
+	// TODO: 関数化
+
+	// トランザクション開始
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 現在のいいね数を取得するクエリを実行する
+	article_id := 1
+	const sqlGetNice = `
+		select nice
+		from articles
+		where article_id = ?;
+	`
+
+	row := tx.QueryRow(sqlGetNice, article_id)
+	if err := row.Err(); err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	// クエリの結果を変数に突っ込む
+	var niceNum int
+	err = row.Scan(&niceNum)
+	if err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	// いいね数を+1
+	const sqlUpdateNice = `update articles set nice = ? where article_id = ?`
+	_, err = tx.Exec(sqlUpdateNice, niceNum + 1, article_id)
+	if err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	tx.Commit()
 }
