38fcc12b022dcb952ac167ffe17f156882347c11
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2025-03-23 23:18:06 +0900

    サービスのコンストラクタを定義しただけ

--- services/service.go
+++ services/service.go
@@ -1,5 +1,12 @@
-// サービス構造体を作成する
+package services
 
-// func main() {
+import "database/sql"
 
-// }
+type MyAppService struct {
+	db *sql.DB
+}
+
+// コンストラクタの定義
+func NewMyAppService(db *sql.DB) *MyAppService {
+	return &MyAppService{db: db}
+}
