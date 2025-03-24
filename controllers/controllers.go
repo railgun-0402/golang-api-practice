1e5b7b79d1d6104e41d5b50a411ea75be547e7d6
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2025-03-24 22:49:05 +0900

    サービス層のリファクタリング

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,9 +1,7 @@
-package handlers
+package controllers
 
 import (
 	"encoding/json"
-	"errors"
-	"fmt"
 	"go-practice-hands/models"
 	"go-practice-hands/services"
 	"io"
@@ -13,54 +11,39 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type MyAppController struct {
+	service *services.MyAppService
+}
+
+// コンストラクター
+func NewMyAppController(s *services.MyAppService) *MyAppController {
+	return &MyAppController{service: s}
+}
+
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello World!\n")
 }
 
-func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-
-	// リクエストヘッダの Content-Length フィールド値を取得
-	length, err := strconv.Atoi(req.Header.Get("Content-Length"))
-	if err != nil {
-		http.Error(w, "cannot get content length\n", http.StatusBadRequest)
-		return
-	}
-	reqBodybuffer := make([]byte, length)
-
-	// RequestBodyの中身を引数「reqBodybuffer」に読み出し
-	// EOFエラーなら正常に読み取り完了
-	if _, err := req.Body.Read(reqBodybuffer); !errors.Is(err, io.EOF) {
-		http.Error(w, "fail to get request body\n", http.StatusBadRequest)
-		return
-	}
-
-	defer req.Body.Close()
+// POST /article のハンドラ
+func (c *MyAppController) PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 
 	var reqArticle models.Article
 	// jsonバイト列をデコード
-	if err := json.Unmarshal(reqBodybuffer, &reqArticle); err != nil {
+	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
 		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
-		return
 	}
 
 	// handler -> service
-	article, err := services.PostArticleService(reqArticle)
+	article, err := c.service.PostArticleService(reqArticle)
 	if err != nil {
 		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
 		return
 	}
-
-	jsonData, err := json.Marshal(article)
-	if err != nil {
-		http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
-		return
-	}
-
-	w.Write(jsonData)
+	json.NewEncoder(w).Encode(article)
 }
 
 // ArticleListHandler GET /article/list のハンドラ
-func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 	// reqから、クエリパラメータの値を取り出したい
 	queryMap := req.URL.Query()
 
@@ -78,23 +61,15 @@ func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 	}
 
 	// handler -> service
-	articleList, err := services.GetArticleListService(page)
+	articleList, err := c.service.GetArticleListService(page)
 	if err != nil {
 		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
 		return
 	}
-
-	jsonData, err := json.Marshal(articleList)
-	if err != nil {
-		errMsg := fmt.Sprintf("fail to encode json (page %d)\n", page)
-		http.Error(w, errMsg, http.StatusInternalServerError)
-		return
-	}
-
-	w.Write(jsonData)
+	json.NewEncoder(w).Encode(articleList)
 }
 
-func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 	// Var: パスパラメータをMapで返却する (map[id: 1234])
 	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
 
@@ -105,29 +80,22 @@ func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 	}
 
 	// handler -> service
-	article, err := services.GetArticleService(articleID)
+	article, err := c.service.GetArticleService(articleID)
 	if err != nil {
 		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
 		return
 	}
-	jsonData, err := json.Marshal(article)
-	if err != nil {
-		errMsg := fmt.Sprintf("fail to encode json (articleID %d)\n", articleID)
-		http.Error(w, errMsg, http.StatusInternalServerError)
-		return
-	}
-
-	w.Write(jsonData)
+	json.NewEncoder(w).Encode(article)
 }
 
 // POST /article/nice のハンドラ
-func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) PostNiceHandler(w http.ResponseWriter, req *http.Request) {
 	var reqArticle models.Article
 	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
 		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
 	}
 
-	article, err := services.PostNiceService(reqArticle)
+	article, err := c.service.PostNiceService(reqArticle)
 	if err != nil {
 		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
 		return
@@ -137,13 +105,13 @@ func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 // POST /comment のハンドラ
-func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) PostCommentHandler(w http.ResponseWriter, req *http.Request) {
 	var reqComment models.Comment
 	if err := json.NewDecoder(req.Body).Decode(&reqComment); err != nil {
 		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
 	}
 
-	comment, err := services.PostCommentService(reqComment)
+	comment, err := c.service.PostCommentService(reqComment)
 	if err != nil {
 		http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
 		return
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"go-practice-hands/handlers"
+	"database/sql"
+	"fmt"
+	"go-practice-hands/controllers"
+	"go-practice-hands/services"
 	"log"
 	"net/http"
 
@@ -9,18 +12,33 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var (
+	dbUser = "user"
+	dbPassword = "pass"
+	dbPort = "3306"
+	dbHost = "localhost"
+	dbName = "sampledb"
+	dbConn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
+)
+
 
 func main() {
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		log.Println("fail to connect DB")
+		return
+	}
+
+	service := services.NewMyAppService(db)
+	con := controllers.NewMyAppController(service)
 	r := mux.NewRouter()
 
-	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article", con.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", con.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", con.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", con.PostNiceHandler).Methods(http.MethodPost)
 
-	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
-
-	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", con.PostCommentHandler).Methods(http.MethodPost)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
