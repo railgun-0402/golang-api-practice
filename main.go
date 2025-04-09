e495016e823e9290e6f25351844eb1fb202679dc
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2025-04-09 23:46:57 +0900

    エラーunwrapしたいだけ

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"go-practice-hands/api"
 	"log"
 	"net/http"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -21,6 +23,14 @@ var (
 
 
 func main() {
+
+	_, err0 := strconv.Atoi("a")
+	fmt.Printf("err0: [%T] %v\n", err0, err0)
+
+	// err0の中に含まれているエラーを取り出し、err1に代入
+	err1 := errors.Unwrap(err0)
+	fmt.Printf("err1: [%T] %v\n", err1, err1)
+
 	db, err := sql.Open("mysql", dbConn)
 	if err != nil {
 		log.Println("fail to connect DB")
