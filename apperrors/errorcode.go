2d41354788f9188c3ce4b029fe55c723d3aebe98
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2025-04-16 23:57:09 +0900

    エラーの種類についてコメントを記載しておく

--- apperrors/errorcode.go
+++ apperrors/errorcode.go
@@ -6,7 +6,10 @@ const (
 	// とりあえず想定外のエラーコードを投入！
 	Unknown ErrCode = "U000"
 
+	// Insert失敗
 	InsertDataFailed ErrCode = "S001"
+	// Select失敗(処理失敗)
 	GetDataFailed ErrCode = "S002"
+	// Select失敗(データ0件)
 	NAData ErrCode = "S003"
 )
